7d412c18565ada61c1cf92dcc2da0bd7ab49d018
Olivier Boudet <olivier.boudet@cooperl.com>
2023-12-06 17:26:46 +0100

    feat: add MONGODB_URI_OPTIONS

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@ use mongodb::{bson::doc, sync::Client};
 use std::env;
 fn main() -> mongodb::error::Result<()> {
     let port = env::var("MONGODB_PORT_NUMBER");
+    let uri_options = env::var("MONGODB_URI_OPTIONS").unwrap_or("".to_string());
     if port.is_err(){
       println!("MONGODB_PORT_NUMBER MISSING!");
       std::process::exit(1);
     }
-    let uri = format!("mongodb://localhost:{}", port.unwrap());
+    let uri = format!("mongodb://localhost:{}/?{}", port.unwrap(), uri_options);
+
+    println!("Connecting to {}", uri);
 
     // Create a new client and connect to the server
     let client = Client::with_uri_str(uri)?;
